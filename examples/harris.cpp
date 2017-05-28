efb9e54c779136a0e7350a5a2306d54967ab2b71
thomas <thomas@gala.com>
2017-05-28 15:08:51 +0100

    Added examples

---
+++ examples/harris.cpp
@@ -0,0 +1,23 @@
+#pragma clite grid(dx, dy, output)
+
+void harris(Image<float> dx, Image<float> dy, Image<float> output, float k){
+
+    float xy = 0.0f;
+    float xx = 0.0f;
+    float yy = 0.0f;
+    
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            float x = dx[idx + i][idy + j];
+            float y = dy[idx + i][idy + j];
+            
+            xy += x*y;
+            xx += x*x;
+            yy += y*y;
+            
+        }
+    }
+    
+    output[idx][idy] = xx*yy - xy*xy - k * (xx + yy) * (xx + yy);
+            
+}
---
+++ examples/sobel.cpp
@@ -0,0 +1,20 @@
+#pragma clite grid(input, dx, dy)
+
+void sobel(Image<float> input, Image<float> dx, Image<float> dy){
+
+    float u1 = input[idx-1][idy-1];
+    float u2 = input[idx][idy-1];
+    float u3 = input[idx+1][idy-1];
+
+    float m1 = input[idx-1][idy];
+    float m3 = input[idx+1][idy];
+
+    float b1 = input[idx-1][idy+1];
+    float b2 = input[idx][idy+1];
+    float b3 = input[idx+1][idy+1];
+
+    dx[idx][idy] = ((u3-u1)+2*(m3-m1)+(b3-b1))/8.0f;
+    dy[idx][idy] = ((b1-u1)+2*(b2-u2)+(b3-u3))/8.0f;
+
+}
+
